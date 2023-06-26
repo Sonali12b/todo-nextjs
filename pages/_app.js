a1d9700a525dba060dcfe50ef228a6f89e727173
Sonali12b <sonali24bhatta@gmail.com>
2023-06-26 16:11:43 +0530

    [done]

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import { NextUIProvider } from '@nextui-org/react';
-
+import '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
   return (
     // 2. Use at the root of your app
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
-import { Container, Text,Input,Button,Card,Spacer, Checkbox } from "@nextui-org/react";
+import {AiFillDelete,AiOutlineFileAdd} from "react-icons/ai";
+import {GrAddCircle} from "react-icons/gr";
 
+import { Container, Text, Input, Button, Card, Spacer, Checkbox } from "@nextui-org/react";
 const TodoApp = () => {
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState("");
@@ -16,24 +18,24 @@ const TodoApp = () => {
     localStorage.setItem("todos", JSON.stringify(todos));
   }, [todos]);
 
-  const addTodo = e => {
+  const addTodo = (e) => {
     e.preventDefault();
     if (!input) return;
     const newTodo = { id: Date.now(), text: input, done: false }; //object literal
     setTodos([...todos, newTodo]);
     setInput("");
-    alert('added!!')
+    alert("added!!");
     console.log("New Todo:", newTodo);
   };
 
-  const deleteTodo = id => {
-    setTodos(todos.filter(todo => todo.id !== id));
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
     localStorage.clear(); // Clear local storage when deleting a todo
   };
 
-  const toggleTodo = id => {
+  const toggleTodo = (id) => {
     setTodos(
-      todos.map(todo => {
+      todos.map((todo) => {
         if (todo.id === id) {
           return { ...todo, done: !todo.done };
         }
@@ -44,93 +46,82 @@ const TodoApp = () => {
 
   const updateTodo = (id, newText) => {
     setTodos(
-      todos.map(todo => (todo.id === id ? { ...todo, text: newText } : todo))
+      todos.map((todo) => (todo.id === id ? { ...todo, text: newText } : todo))
     );
   };
 
   return (
-    <Container css={{ p: "2rem", background: "$pink100", minHeight:"100vh", w:"70%" }}>
-      <Text
-        h1
-        size={60}
-        css={{
-          textGradient: "45deg, #080F61 0%, #FF1A16 100%",
-        }}
-        weight="bold"
-      >
+    <>
+    <Container >
+      <Text h3 css={{ textGradient: "45deg, #080F61 0%, #FF1A16 100%" }} weight="bold">
         Todo App
       </Text>
-      <Card css={{p:"2rem"}}>
-      <form onSubmit={addTodo} >
-        <Input
-          type="text"
-          value={input}
-          onChange={e => setInput(e.target.value)}
-          placeholder="Add a new todo"
-        />
-        <Spacer y={0.5} />
-        <Button
-          type="submit"
-          gradient
-          auto
-          css={{
-            borderRadius: "$xs",
-            boxShadow: "$sm",
-          }}
-        >
-          Add Todo
-        </Button>
-      </form>
+    </Container>
+    <Container>
+      <Card css={{ p: "2rem", background:"#99DBF5" }}>
+        <form onSubmit={addTodo}>
+          <Input
+          underlined
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Add a new todo"
+          /><GrAddCircle css={{}} onClick={addTodo}/>
+        </form>
       </Card>
-      <Container css={{p:"2rem" }}>
-      <ul>
-        {todos.map(todo => (
-          <li
-            key={todo.id}
-            className={`todo-item ${todo.done ? "done" : ""}`}
-            css={{
-              display: "flex",
-              alignItems: "center",
-              gap: "0.5rem",
-            }}
-          >
-            {/* <Input
-              type="checkbox"
-              checked={todo.done}
-              onChange={() => toggleTodo(todo.id)}
-            /> */}
-            <Checkbox defaultSelected size="xs" checked={todo.done}
-            onChange={() => toggleTodo(todo.id)}>
-            
-          </Checkbox>
-          {/* <Spacer /> */}
-            <Input
-              underlined
-              type="text"
-              value={todo.text}
-              onChange={e => updateTodo(todo.id, e.target.value)}
-            />
-            <Button
-              className="delete"
-              auto
-              // color="error"
-              ghost
-              onClick={() => deleteTodo(todo.id)}
-              css={{
-                opacity: 0.7,
-                p:"2px",
-                color:"red",
-                border:"none"
-              }}
-            >
-              Delete
-            </Button>
-          </li>
-        ))}
-          <Spacer y={1.5} />
-      </ul>
+      <Container css={{ p: "1rem", background: "#9AC5F4",mt:"2rem",borderRadius:"1rem"}}>
+        {todos.length === 0 ? (
+          <Text h3 css={{ textGradient: "45deg, #080F61 0%, #FF1A16 100%" }}>No tasks pending</Text>
+        ) : (
+          <ul>
+            {todos.map((todo) => (
+              <li
+                key={todo.id}
+                className={`todo-item ${todo.done ? "done" : ""}`}
+                css={{
+                  display: "flex",
+                  alignItems: "center",
+                  gap: "0.5rem",
+                  w:"100%"
+                }}
+              >
+                <Checkbox
+                  size="xs"
+                  checked={todo.done}
+                  onChange={() => toggleTodo(todo.id)}
+                ></Checkbox>
+                <Input
+                  underlined
+                  type="text"
+                  value={todo.text}
+                  onChange={(e) => updateTodo(todo.id, e.target.value)}
+                /><AiFillDelete onClick={() => deleteTodo(todo.id)}/>
+                {/* <button
+                  className="delete"
+                  auto
+                  ghost
+                  onClick={() => deleteTodo(todo.id)}
+                  css={{
+                    opacity: 0.7,
+                    p: "2px",
+                    color: "red",
+                    border: "none",
+                    "&:hover": {
+                      background: "$pink100",
+                      color: "$pink800",
+                    },
+                  }}
+                >
+                  Delete
+                </button> */}
+              </li>
+            ))}
+            <Spacer y={1.5} />
+          </ul>
+        )}
       </Container>
     </Container>
+    </>
   );
 };
 
